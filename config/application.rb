10a481cde0f864e3d573e628dfc9ac558b052f1f
tjallen27 <tjallen91@hotmail.co.uk>
2017-05-22 12:36:11 +0100

    gem update

--- config/application.rb
+++ config/application.rb
@@ -27,13 +27,6 @@ module BlogApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
-
     config.eager_load_paths << Rails.root.join('lib')
   end
 end
